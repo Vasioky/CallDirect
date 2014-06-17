40b4838e228678275b57a07c895d7354113a2b94
Vasile Postolache <vaioky@mail.ru>
2014-06-17 10:26:33 +0300

    added extention content

---
+++ CallDirect/app/background.js
@@ -0,0 +1,10 @@
+﻿console.log('test');
+chrome.contextMenus.create({
+    "title": "Call This",
+    "contexts": ["page", "selection", "link"],
+    "onclick": clickHandler
+});
+
+function clickHandler() {
+    cosole.log('click');
+}
